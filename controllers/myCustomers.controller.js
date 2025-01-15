620231b1488de4803bf4b35e31ffbff295750e60
Somiha Tasnim <somihatasnim@Somihas-MacBook-Pro.local>
2025-01-15 08:47:28 +0600

    update my customer

--- controllers/myCustomers.controller.js
+++ controllers/myCustomers.controller.js
@@ -28,102 +28,59 @@ exports.myCustomers = async (req, res) => {
       );
 
       const myCustomers = await queryAsync(
-        "SELECT * FROM `user` WHERE `own_ref_id` = ?",
-        [userDetails[0].reference_id]
+        "SELECT * FROM `user` WHERE `reference_id` = ?",
+        [userDetails[0].own_ref_id]
       );
 
       const customerIds = myCustomers.map((customer) => customer.user_id);
+
+      const coustomersShopIds = await queryAsync(
+        "SELECT id, seller_user_id FROM `shop` WHERE `seller_user_id` IN (?)",
+        [customerIds]
+      );
+
+      const customerShopIds = coustomersShopIds.map((shop) => shop.id);
+
+      const myEarning = await queryAsync(
+        "SELECT ref_id, SUM(amount) as total_amount, COUNT(*) as count FROM `fund_transfer` WHERE `ref_id` IN (?) AND `receiver_id` = ? GROUP BY ref_id",
+        [customerShopIds, seller_id]
+      );
 
       const customersCompletedOrders = await queryAsync(
         "SELECT user_id, COUNT(*) as completedOrders FROM `orders` WHERE `user_id` IN (?) AND `order_status` = 1 GROUP BY user_id",
         [customerIds]
       );
 
-      db.query(
-        "SELECT * FROM `shop` WHERE `id` = ?",
-        [seller_id],
-        (err1, res1) => {
-          if (!err1) {
-            db.query(
-              "SELECT * FROM `user` WHERE `reference_id` = ?",
-              [res1[0].shop_number],
-              (err2, res2) => {
-                if (!err2) {
-                  const customerIds = res2.map((customer) => customer.user_id);
+      const customersWithOrders = myCustomers.map((customer) => {
+        const orders = customersCompletedOrders.find(
+          (order) => order.user_id === customer.user_id
+        );
+        const customerSellerId = coustomersShopIds.find(
+          (shop) => shop.seller_user_id === customer.user_id
+        );
+        const earning = myEarning.find(
+          (earning) => earning.ref_id === customerSellerId.id
+        );
+        return {
+          ...customer,
+          completedOrders: orders ? orders.completedOrders : 0,
+          customerSellerId: customerSellerId ? customerSellerId.id : null,
+          earning: earning ? earning.total_amount : 0,
+          reveived: earning ? earning.count : 0,
+        };
+      });
 
-                  if (customerIds.length > 0) {
-                    db.query(
-                      "SELECT user_id, COUNT(*) as completedOrders FROM `orders` WHERE `user_id` IN (?) AND `order_status` = 1 GROUP BY user_id",
-                      [customerIds],
-                      (err3, res3) => {
-                        if (!err3) {
-                          // Merge completed orders count into customer data
-                          const customersWithOrders = res2.map((customer) => {
-                            const orders = res3.find(
-                              (order) => order.user_id === customer.user_id
-                            );
-                            return {
-                              ...customer,
-                              completedOrders: orders
-                                ? orders.completedOrders
-                                : 0,
-                            };
-                          });
-
-                          db.query(
-                            "SELECT * FROM `payment_history` WHERE `shop_id` = ? AND `type` = 2",
-                            [seller_id],
-                            (err4, res4) => {
-                              if (!err4) {
-                                res.render("myCustomers", {
-                                  ogImage:
-                                    "https://admin.save71.com/images/logo-og.webp",
-                                  ogTitle:
-                                    "Save71 Connects You and the World through Business.",
-                                  ogUrl: "https://admin-save71.lens-ecom.store",
-                                  currencyCode,
-                                  currRate,
-                                  menuId: "myCustomers",
-                                  name: "My Customers",
-                                  customers: customersWithOrders,
-                                  earning: res4,
-                                  notification: notification,
-                                });
-                              } else {
-                                res.send(err4);
-                              }
-                            }
-                          );
-                        } else {
-                          res.send(err3);
-                        }
-                      }
-                    );
-                  } else {
-                    res.render("myCustomers", {
-                      ogImage: "https://admin.save71.com/images/logo-og.webp",
-                      ogTitle:
-                        "Save71 Connects You and the World through Business.",
-                      ogUrl: "https://admin-save71.lens-ecom.store",
-                      currencyCode,
-                      currRate,
-                      menuId: "myCustomers",
-                      name: "My Customers",
-                      customers: res2,
-                      earning: [],
-                      notification: notification,
-                    });
-                  }
-                } else {
-                  res.send(err2);
-                }
-              }
-            );
-          } else {
-            res.send(err1);
-          }
-        }
-      );
+      return res.render("myCustomers", {
+        ogImage: "https://admin.save71.com/images/logo-og.webp",
+        ogTitle: "Save71 Connects You and the World through Business.",
+        ogUrl: "https://admin-save71.lens-ecom.store",
+        currencyCode,
+        currRate,
+        menuId: "myCustomers",
+        name: "My Customers",
+        response: customersWithOrders,
+        notification: notification,
+      });
     } catch (err) {
       console.error(err);
       res.status(500).send("Internal Server Error");
